a66ce1fd41bb95fb8e277c8d0b544551c87b4b06
Wallace Vilela <wallace.vilela9@gmail.com>
2023-08-27 15:12:00 -0300

    Initial commit

---
+++ src/MyEcommerceApp.Core/MyEcommerceAppConsts.cs
@@ -0,0 +1,20 @@
+﻿using MyEcommerceApp.Debugging;
+
+namespace MyEcommerceApp
+{
+    public class MyEcommerceAppConsts
+    {
+        public const string LocalizationSourceName = "MyEcommerceApp";
+
+        public const string ConnectionStringName = "Default";
+
+        public const bool MultiTenancyEnabled = true;
+
+
+        /// <summary>
+        /// Default pass phrase for SimpleStringCipher decrypt/encrypt operations
+        /// </summary>
+        public static readonly string DefaultPassPhrase =
+            DebugHelper.IsDebug ? "gsKxGZ012HLL3MI5" : "0d59f5544b31455cb29356d3333b4b31";
+    }
+}
