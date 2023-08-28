7c672ace7487c88a1a2b78c657c6529de0d1e762
Wallace Vilela <wallace.vilela9@gmail.com>
2023-08-28 16:29:27 -0300

    First DbSets and migration for Product class

---
+++ src/MyEcommerceApp.Application/App/v1/Products/ProductService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEcommerceApp.App.v1.Products
+{
+    public class ProductService : IProductService
+    {
+        public Task CreateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
